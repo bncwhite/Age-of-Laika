5b7cd51ea09189cce0715d39b2f639853a5202cb
Bradley White <codebybrad@icloud.com>
2014-08-14 10:37:48 -0500

    Initial Commit

---
+++ Age of Laika/BWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BWAppDelegate.h
+//  Age of Laika
+//
+//  Created by Bradley White on 8/14/14.
+//  Copyright (c) 2014 Bradley White. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
